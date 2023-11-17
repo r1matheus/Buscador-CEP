996eeb8e7e93aa82a389ff6b8ff0df6d540e7399
r1matheus <russomatheus15@gmail.com>
2023-11-17 11:09:19 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+import api from "./services/api"
+
+function App() {
+
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState('');
+
+  async function handleSearch() {
+    if (input == '') {
+      alert("Preencha algum cep")
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data)
+      setInput('')
+
+    } catch {
+      alert("Erro ao buscar")
+      setInput("")
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador Cep</h1>
+
+      <div className="containerInput">
+        <input type="text"
+          placeholder="Digite seu Cep"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color='#FFF' />
+        </button>
+
+      </div>
+
+
+      {Object.keys(cep).length > 0 && (
+        <main className='main'>
+          <h2>CEP: {cep.cep}</h2>
+          <span>Rua: {cep.logradouro}</span>
+          <span>Complemento: {cep.complemento}</span>
+          <span>Bairro: {cep.bairro}</span>
+          <span>Cidade: {cep.localidade} - {cep.uf}</span>
+        </main>
+      )}
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;500&display=swap');
+</style>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
